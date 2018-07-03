ab1e80ce0d9f1a28f96420bf1bd74c64992d5c2c
trungpro2320 <danganhthanhtrung@gmail.com>
2018-07-03 13:35:46 +0700

    add status

--- js/offline.js
+++ js/offline.js
@@ -4,9 +4,7 @@
   //After DOM Loaded
   document.addEventListener('DOMContentLoaded', function(event) {
     //On initial load to check connectivity
-    if (!navigator.onLine) {
       updateNetworkStatus();
-    }
 
     window.addEventListener('online', updateNetworkStatus, false);
     window.addEventListener('offline', updateNetworkStatus, false);
