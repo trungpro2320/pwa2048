7b8759e457717a0339386d43a1bc97aa55a76e81
trungpro2320 <danganhthanhtrung@gmail.com>
2018-07-03 09:54:52 +0700

    add manifest

--- js/application.js
+++ js/application.js
@@ -2,3 +2,15 @@
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
+
+function componentDidMount() {
+    window.addEventListener('online', () => this.setOnlineStatus(true));
+    window.addEventListener('offline', () => this.setOnlineStatus(false));
+  }
+
+function componentWillUnmount() {
+    window.removeEventListener('online');
+    window.removeEventListener('offline');
+  }
+
+var setOnlineStatus = isOnline => this.setState({ online: isOnline })
