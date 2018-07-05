46706760bb73b42971f9523f397614107f94c9f2
trungpro2320 <danganhthanhtrung@gmail.com>
2018-07-05 10:22:08 +0700

    change sw

--- service_worker_main.js
+++ service_worker_main.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const CACHE_NAME = 'cache-v1';
-const RUNTIME = 'runtime';
 // The files we want to cache
 const resourceList = [
   '/',
@@ -36,48 +35,24 @@ const resourceList = [
 ];
 
 //This is the "Offline copy of pages" wervice worker
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(resourceList))
-      .then(self.skipWaiting())
-  );
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function(cache) {
+			return cache.addAll(resourceList).then(function() {
+				return self.skipWaiting();
+			});
+		})
+	);
 });
-//Cache list of predefined resources during activate event, using the cache API
-self.addEventListener('activate', function( event ){
-   const currentCaches = [CACHE_NAME, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+self.addEventListener('activate', function(event) {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
 });
